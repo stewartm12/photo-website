d493d000d59e2d420fda87ed44b6a578f0a4519b
stewart <stewartmorales129@gmail.com>
2025-06-17 13:20:05 -0500

    implement stores rspecs

--- services/api/spec/mailers/invitation_mailer_spec.rb
+++ services/api/spec/mailers/invitation_mailer_spec.rb
@@ -1,5 +1,17 @@
 require "rails_helper"
 
 RSpec.describe InvitationMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '.invite' do
+    let(:user) { create(:user) }
+    let(:store) { create(:store) }
+    let!(:membership) { create(:store_membership, user: user, store: store) }
+    let(:invited_email) { 'invited@email.com' }
+
+    it 'sends an invitation email to the invitee' do
+      email = InvitationMailer.invite(user, invited_email, store).deliver_now
+
+      expect(email.to).to eq([invited_email])
+      expect(email.subject).to eq("#{user.full_name} invited you to join their store")
+    end
+  end
 end
