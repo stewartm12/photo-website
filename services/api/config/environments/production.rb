d5be6d4dc8c9a6db9302a4629a8ecc021c02d790
stewart <stewartmorales129@gmail.com>
2025-06-20 11:46:32 -0500

    set up procfile

--- services/api/config/environments/production.rb
+++ services/api/config/environments/production.rb
@@ -67,7 +67,7 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
 
   # Set host to be used by links generated in mailer templates.
-  config.action_mailer.default_url_options = { host: ENV['API_DOMAIN'] }
+  config.action_mailer.default_url_options = { host: ENV['APP_DOMAIN'] }
 
   # Specify outgoing SMTP server. Remember to add smtp/* credentials via rails credentials:edit.
   # config.action_mailer.smtp_settings = {
--- services/api/db/schema.rb
+++ services/api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_06_19_224821) do
+ActiveRecord::Schema[8.0].define(version: 2025_06_20_221947) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
   enable_extension "pg_trgm"
@@ -229,6 +229,17 @@ ActiveRecord::Schema[8.0].define(version: 2025_06_19_224821) do
     t.integer "photos_count", default: 0, null: false
     t.index ["store_id", "name"], name: "index_showcases_on_store_id_and_name", unique: true
     t.index ["store_id"], name: "index_showcases_on_store_id"
+  end
+
+  create_table "solid_cache_entries", force: :cascade do |t|
+    t.binary "key", null: false
+    t.binary "value", null: false
+    t.datetime "created_at", null: false
+    t.bigint "key_hash", null: false
+    t.integer "byte_size", null: false
+    t.index ["byte_size"], name: "index_solid_cache_entries_on_byte_size"
+    t.index ["key_hash", "byte_size"], name: "index_solid_cache_entries_on_key_hash_and_byte_size"
+    t.index ["key_hash"], name: "index_solid_cache_entries_on_key_hash", unique: true
   end
 
   create_table "store_memberships", force: :cascade do |t|
