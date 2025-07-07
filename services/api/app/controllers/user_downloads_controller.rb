b8ef7da88fb09192ed724e777e95c1a2fa2092b3
stewart <stewartmorales129@gmail.com>
2025-07-07 15:39:31 -0500

    create a user download cleanup task

--- services/api/app/controllers/user_downloads_controller.rb
+++ services/api/app/controllers/user_downloads_controller.rb
@@ -2,19 +2,24 @@ class UserDownloadsController < ApplicationController
   include Pagy::Backend
 
   def index
-    @pagy, @downloads = pagy(Current.user.user_downloads.includes(:collection).order(created_at: :desc))
+    @pagy, @downloads = pagy(
+      Current.user.user_downloads
+      .not_expired
+      .includes(:collection)
+      .order(created_at: :desc)
+    )
   end
 
   def show
     download = Current.user.user_downloads.find(params[:id])
 
     if download.expired? || !File.exist?(download.file_path)
-      redirect_to store_user_downloads_path(Current.store,), alert: "This download is no longer available."
+      redirect_to store_user_downloads_path(Current.store,), alert: 'This download is no longer available.'
     else
       send_file download.file_path,
                 filename: File.basename(download.file_path),
-                type: "application/zip",
-                disposition: "attachment"
+                type: 'application/zip',
+                disposition: 'attachment'
     end
   end
 end
