833cbeb79ee06af29f5f460885bac5d1a0a9f42d
stewart <stewartmorales129@gmail.com>
2025-07-09 23:12:11 -0500

    implement membership removal

--- services/api/app/controllers/store_memberships_controller.rb
+++ services/api/app/controllers/store_memberships_controller.rb
@@ -1,4 +1,6 @@
 class StoreMembershipsController < ApplicationController
+  include Pagy::Backend
+
   skip_before_action :set_current_store_from_slug, only: %i[show]
 
   def show
@@ -23,5 +25,28 @@ class StoreMembershipsController < ApplicationController
   def create
     Current.user.send_invitation_email(params[:email], Current.store)
     flash.now[:notice] = 'Invitation email sent successfully!'
+  end
+
+  def destroy
+    store_membership = Current.store.store_memberships.find_by(id: params[:id])
+    store_membership.destroy
+    flash[:success] = 'Member removed successfully!'
+
+    @store = Current.store
+    @pagy, @store_memberships = pagy(filtered_memberships(@store))
+  end
+
+  private
+
+  def filtered_memberships(store)
+    store_memberships = store.store_memberships.includes(:user).joins(:user)
+
+    if params[:name].present?
+      store_memberships = store_memberships.where(
+        'users.first_name ILIKE :name OR users.last_name ILIKE :name', name: "%#{params[:name]}%"
+      )
+    end
+
+    store_memberships.order(created_at: :asc)
   end
 end
