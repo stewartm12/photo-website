e1cb8c4631ea654bf5642ef797d10990227bc1da
stewart <stewartmorales129@gmail.com>
2025-08-02 15:05:04 -0500

    update all emails to contain more content

--- services/api/spec/mailers/appointment_mailer_spec.rb
+++ services/api/spec/mailers/appointment_mailer_spec.rb
@@ -6,6 +6,13 @@ RSpec.describe AppointmentMailer, type: :mailer do
     let(:gallery) { Gallery.first }
     let(:appointment) { create(:appointment, customer: customer, store: Store.first) }
     let!(:appointment_package) { create(:appointment_package, appointment: appointment) }
+    let!(:photo) do
+      create(
+        :photo,
+        imageable: Store.first,
+        image: Rack::Test::UploadedFile.new("#{Rails.root}/spec/fixtures/files/test2.jpg")
+      )
+    end
 
     it 'sends the email to the customer and includes the correct BCC' do
       email = AppointmentMailer.new_appointment_email(appointment, customer, Store.first).deliver_now
