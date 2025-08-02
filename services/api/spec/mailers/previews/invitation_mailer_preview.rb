82ccafb0f651c0a81f3eeed5a87777688a7719bb
stewart <stewartmorales129@gmail.com>
2025-08-02 15:05:04 -0500

    update all emails to contain more content

--- services/api/spec/mailers/previews/invitation_mailer_preview.rb
+++ services/api/spec/mailers/previews/invitation_mailer_preview.rb
@@ -1,4 +1,8 @@
 # Preview all emails at http://localhost:3000/rails/mailers/invitation_mailer_mailer
 class InvitationMailerPreview < ActionMailer::Preview
-
+  # Preview this email at http://localhost:3001/rails/mailers/invitation_mailer/invite
+  def invite
+    user = User.first
+    InvitationMailer.invite(user, 'new-user@email.com', user.stores.first)
+  end
 end
